df39aa37389e5dbb9e6eafc03521ab66a4cdb65d
test <name@example,com>
2021-03-20 11:27:38 +0800

    1.3.14 修改签名

--- Algs/src/chapter1Section3/collection/exercise/ResizingArrayQueueOfString.java
+++ Algs/src/chapter1Section3/collection/exercise/ResizingArrayQueueOfString.java
@@ -7,10 +7,10 @@ import java.util.Iterator;
  * @deprecated 编写一个程序 `ResizingArrayQueueOfString`，使用定长数组实现队列的抽象，然后扩展实现，使用调整数组的方法突破大小限制
  */
 public class ResizingArrayQueueOfString<Item> implements Iterator<Item> {
-    Item[] items;
-    int maxSize;
-    int first;
-    int last;
+    private Item[] items;
+    private int maxSize;
+    private int first;
+    private int last;
 
     public ResizingArrayQueueOfString(int maxSize){
         items = (Item[]) new Object[maxSize];
