396cdc9095cf3d5ab31e19e22643fbc81decf8e9
test <name@example,com>
2021-03-31 15:19:24 +0800

    更新1.3.36

--- Algs/src/chapter1Section3/collection/exercise/RandomQueue.java
+++ Algs/src/chapter1Section3/collection/exercise/RandomQueue.java
@@ -3,6 +3,8 @@ package chapter1Section3.collection.exercise;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.Iterator;
+
 /**
  * @author pachi
  * @deprecated 随机队列。删除一个元素时，随机交换某个元素和末位置元素位置，之后像 `ResizingArrayStack` 一样删除并返回末位置元素。编写一个用例，使用 `RandomQueue<Card>` 在桥牌中发牌（每个人13张）
@@ -42,6 +44,10 @@ public class RandomQueue<Item> {
         return res;
     }
 
+    public int size(){
+        return queueLength;
+    }
+
     private void resize(int length) {
         Item[] temp = (Item[]) new Object[length];
         if (queueLength >= 0) System.arraycopy(item, 0, temp, 0, queueLength);
@@ -52,6 +58,27 @@ public class RandomQueue<Item> {
         return item[StdRandom.uniform(queueLength)];
     }
 
+    public Iterator<Item> iterator(){
+        return new ArrayIterator();
+    }
+
+    private class ArrayIterator implements Iterator<Item>{
+        private int len = queueLength;
+
+        @Override
+        public boolean hasNext() {
+            return queueLength > 0;
+        }
+
+        @Override
+        public Item next() {
+            int index = StdRandom.uniform(len);
+            Item tmp = item[index];
+            item[index] = item[queueLength--];
+            return tmp;
+        }
+    }
+
     public static class Card {
         int value;
         int type;
@@ -59,6 +86,7 @@ public class RandomQueue<Item> {
 
     public static void main(String[] args) {
         RandomQueue<Card> cards = new RandomQueue<>();
+        RandomQueue<Card> test = new RandomQueue<>();
 
 
         for (int value = 1; value <= 13; value++)
@@ -67,6 +95,14 @@ public class RandomQueue<Item> {
                 c.value = value;
                 c.type = type;
                 cards.enqueue(c);
+            }
+
+        for (int value = 1; value <= 13; value++)
+            for (int type = 1; type <= 4; type++) {
+                Card c = new Card();
+                c.value = value;
+                c.type = type;
+                test.enqueue(c);
             }
 
         StdOut.printf("---Format:(CardValue,CardType)---\n");
@@ -93,6 +129,13 @@ public class RandomQueue<Item> {
             Card c = cards.dequeue();
             StdOut.print("(" + c.value + "," + c.type + ")");
         }
+
+        Iterator<Card> iterator = test.iterator();
+        while (iterator.hasNext()) System.out.print(iterator.next() + " ");
+        System.out.println();
+        for (int i = 0; i < test.size(); i++)
+            System.out.print(test.dequeue() + " ");
+
     }
 }
 
