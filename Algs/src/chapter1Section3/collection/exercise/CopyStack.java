ec83aff58a4b3d78320876178e1f80e26a2f427b
test <name@example,com>
2021-04-10 15:46:20 +0800

    1.3.42

---
+++ Algs/src/chapter1Section3/collection/exercise/CopyStack.java
@@ -0,0 +1,27 @@
+package chapter1Section3.collection.exercise;
+
+import java.util.Stack;
+
+/**
+ * @author pachi
+ * @deprecated 复制栈，为基于链表实现的栈编写一个新的构造函数
+ * @param <Item>
+ */
+public class CopyStack<Item> extends Stack<Item> {
+    public CopyStack(CopyStack<Item> s){
+       CopyStack<Item> stack = new CopyStack<>();
+
+       int size = stack.size();
+
+       Item val;
+        for (int i = 0; i < size; i++) {
+            val = s.peek();
+            this.push(val);
+            stack.push(val)
+        }
+
+        s = stack;
+    }
+    public CopyStack(){}
+
+}
