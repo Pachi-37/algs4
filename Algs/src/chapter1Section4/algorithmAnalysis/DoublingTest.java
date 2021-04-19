61c677a5c6a043a2cfc944fcf53c72599a96a262
test <name@example,com>
2021-04-19 17:11:33 +0800

    1.4 算法分析

---
+++ Algs/src/chapter1Section4/algorithmAnalysis/DoublingTest.java
@@ -0,0 +1,29 @@
+package chapter1Section4.algorithmAnalysis;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.Stopwatch;
+
+public class DoublingTest {
+    public static double timeTrial(int N) {
+        // 为处理 N 个随机的六位整数的 ThreeSum.count() 计时
+        int MAX = 1000000;
+        int[] a = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(-MAX, MAX);
+        }
+
+        Stopwatch timer = new Stopwatch();
+        int cnt = ThreeSum.count(a);
+        return timer.elapsedTime();
+    }
+
+    public static void main(String[] args) {
+        for (int N = 250; true; N += N) {
+            // 打印问题为 N 规模时程序的用时
+            double time = timeTrial(N);
+            StdOut.printf("%7d %5.1f\n", N, time);
+        }
+    }
+}
