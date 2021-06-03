9a937aab3595d4e890b359f19e2ea6f878a2b82b
test <name@example,com>
2021-06-03 09:09:00 +0800

    Linear Search

---
+++ Algs/src/com/immoc/linear_search/LinearSearch.java
@@ -0,0 +1,27 @@
+package com.immoc.linear_search;
+
+/**
+ * @author pachi
+ */
+public class LinearSearch {
+    public int search(int[] data, int target) {
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        int[] data = {24, 18, 12, 9, 16, 66, 32, 4};
+
+        LinearSearch search = new LinearSearch();
+        int res = search.search(data, 16);
+        System.out.println(res);
+
+        search.search(data,1);
+    }
+}
