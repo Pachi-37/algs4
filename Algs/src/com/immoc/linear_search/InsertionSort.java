adb55a926176843c69f2b2af37569aa1340e40e3
pachi <1772343187@qq.com>
2021-06-15 19:40:15 +0800

    插入排序

--- Algs/src/com/immoc/linear_search/InsertionSort.java
+++ Algs/src/com/immoc/linear_search/InsertionSort.java
@@ -1,4 +1,39 @@
 package com.immoc.linear_search;
 
 public class InsertionSort {
+
+    private InsertionSort() {
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j].compareTo(arr[i]) >= 0) {
+                    insertTo(arr, j, i);
+                }
+            }
+        }
+    }
+
+    private static <E> void insertTo(E[] arr, int order, int index) {
+        E tmp = arr[index];
+
+        if (index - order >= 0) System.arraycopy(arr, order, arr, order + 1, index - order);
+        if (order + 1 < index) {
+            arr[order + 1] = tmp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 100;
+        Integer[] arr = ArrayGenerator.generateRandomArray(n, n);
+
+        InsertionSort.sort(arr);
+
+        for (Integer e :
+                arr) {
+            System.out.println(e);
+        }
+    }
 }
