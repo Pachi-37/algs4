66699d4d3bdb74d6f0beccb7bb58c7628315799f
pachi <1772343187@qq.com>
2021-07-08 15:33:42 +0800

    链表实现队列

---
+++ Algs/src/com/immoc/queue/LinkedListQueue.java
@@ -0,0 +1,122 @@
+package com.immoc.queue;
+
+/**
+ * @author pachi
+ */
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private class Node {
+        public E e;
+        Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head, tail;
+    private int size;
+
+    public LinkedListQueue() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+
+        if (tail == null) {
+            tail = new Node(e);
+            head = tail;
+        } else {
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Can not dequeue, the queue is empty.");
+        }
+
+        Node retNode = head;
+        head = head.next;
+        retNode.next = null;
+
+        if (head == null) {
+            tail = null;
+        }
+
+        size--;
+
+        return retNode.e;
+    }
+
+    @Override
+    public E getFront() {
+
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is Empty.");
+        }
+
+        return head.e;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: front ");
+
+        Node cur = head;
+        while (cur != null) {
+            res.append(cur);
+            res.append("->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+
+        for (int i = 0; i < 20; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+        }
+
+        for (int i = 0; i < 20; i++) {
+            queue.dequeue();
+            System.out.println(queue);
+        }
+    }
+}
