5392d9c0cc29dde771c78bffabab0d3c4ae9d45b
pachi <1772343187@qq.com>
2021-07-25 09:33:00 +0800

    优化代码，使用temp数组存放元素

--- Algs/src/com/imooc/sort/merge/MergeSort.java
+++ Algs/src/com/imooc/sort/merge/MergeSort.java
@@ -1,6 +1,5 @@
 package com.imooc.sort.merge;
 
-import com.immoc.array.Array;
 import com.immoc.linear_search.ArrayGenerator;
 import com.immoc.linear_search.SortingHelper;
 import com.immoc.utils.GenerateDepthString;
@@ -16,10 +15,11 @@ public class MergeSort {
     }
 
     public static <E extends Comparable<E>> void sort(E[] arr) {
-        sort(arr, 0, arr.length - 1, 0);
+        E[] temp = Arrays.copyOf(arr, arr.length);
+        sort(arr, 0, arr.length - 1, 0, temp);
     }
 
-    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r, int depth) {
+    private static <E extends Comparable<E>> void sort(E[] arr, int l, int r, int depth, E[] temp) {
 
         String depthString = GenerateDepthString.generate(depth);
         System.out.println(depthString + String.format("mergeSort arr [%d, %d]", l, r));
@@ -29,13 +29,13 @@ public class MergeSort {
         }
 
         int mid = (l + r) / 2;
-        sort(arr, l, mid, depth + 1);
-        sort(arr, mid + 1, r, depth + 1);
+        sort(arr, l, mid, depth + 1, temp);
+        sort(arr, mid + 1, r, depth + 1, temp);
 
         if (arr[mid].compareTo(arr[mid + 1]) > 0) {
             System.out.print(depthString);
             System.out.println(String.format("merge arr [%d, %d] and arr [%d, %d]", l, mid, mid + 1, r));
-            merge(arr, l, mid, r);
+            merge(arr, l, mid, r, temp);
         }
 
         System.out.print(depthString);
@@ -56,26 +56,25 @@ public class MergeSort {
      * @param r   较大索引
      * @param <E> 泛型
      */
-    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r) {
+    private static <E extends Comparable<E>> void merge(E[] arr, int l, int mid, int r, E[] temp) {
 
-        E[] temp = Arrays.copyOfRange(arr, l, r + 1);
-
+        System.arraycopy(arr, l, temp, l, r - l + 1);
         int i = l, j = mid + 1;
 
         // 每轮循环为 arr[k] 赋值
         for (int k = l; k <= r; k++) {
 
             if (i > mid) {
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j++;
             } else if (j > r) {
-                arr[k] = temp[i - l];
+                arr[k] = temp[i];
                 i++;
-            } else if (temp[i - l].compareTo(temp[j - l]) <= 0) {
-                arr[k] = temp[i - l];
+            } else if (temp[i].compareTo(temp[j]) <= 0) {
+                arr[k] = temp[i];
                 i++;
             } else {
-                arr[k] = temp[j - l];
+                arr[k] = temp[j];
                 j++;
             }
         }
